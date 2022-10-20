2777926f940b4442e225c24f4a96ca7e32188307
Kimi <kimiing@outlook.com>
2022-10-20 16:33:29 +0800

    hot-fix1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,1 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello Kimi!");
-        System.out.println("Hello Kimi!");
-        System.out.println("Hello Kimi!");
-    }
-}
+public class Main {    public static void main(String[] args) {        System.out.println("Hello Kimi!");        System.out.println("Hello Kimi!");        System.out.println("Hello Kimi!");        System.out.println("Hello Kimi! hot-fix");    }}
